f9ffd4318a3d9ed1b9f599a3e1fd1e4c184d30f2
Ramanan-Bala <ramanan.bala2003@gmail.com>
2026-02-20 22:53:59 +0530

    - moving UI api url to env

--- cashly-next/src/lib/api/axios.ts
+++ cashly-next/src/lib/api/axios.ts
@@ -8,7 +8,7 @@ declare module "axios" {
 }
 
 const api = axios.create({
-  baseURL: "http://localhost:8080/api",
+  baseURL: process.env.API_URL || "http://localhost:8080/api",
   headers: {
     "Content-Type": "application/json",
   },
