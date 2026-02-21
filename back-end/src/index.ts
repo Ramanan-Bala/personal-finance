fe77da549b18b2c14c12b34dd26782ac4816c7d8
Ramanan-Bala <ramanan.bala2003@gmail.com>
2026-02-21 23:35:23 +0530

    - added UI_ORIGIN to .env.example - added CORS middleware to index.ts

--- back-end/src/index.ts
+++ back-end/src/index.ts
@@ -8,15 +8,12 @@ dotenv.config();
 
 const app = express();
 const port = process.env.PORT || 8080;
+const UI_ORIGIN = process.env.UI_ORIGIN || 'http://localhost:3000';
 
 app.use(helmet());
 app.use(
   cors({
-    origin: [
-      'http://localhost:3000',
-      'http://localhost:5173',
-      'https://personal-finance-psi-orpin.vercel.app',
-    ],
+    origin: [UI_ORIGIN],
     credentials: true,
   }),
 );
